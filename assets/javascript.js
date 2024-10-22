82e585f9dc8ce4bba781dd18edad386a9d87808b
sahilalirza <sahilalirza25@gmail.com>
2024-10-22 17:40:57 +0530

    JS TASK

---
+++ assets/javascript.js
@@ -0,0 +1,100 @@
+
+console.log("Arithmatic Operator");
+console.log("-------------------");
+
+
+let add = 12 + 8;
+console.log("Addition is",add);
+
+let sub = 20 - 5;
+console.log("Subtraction is",sub);
+
+let mul = 6 * 7;
+console.log("Multiplication is",mul);
+
+let div = 24 / 4;
+console.log("Divide is",div);
+
+console.log("Comaparison Operator");
+console.log("-------------------");
+
+console.log("10 > 5",10>5);
+console.log("10 < 5",10<5);
+console.log("10 <= 5",10<=5);
+console.log("10 >= 5",10>=5);
+console.log('"10" == 10',"10" == 10);
+console.log('"10" === 10',"10" === 10);
+console.log("false != 0", false!=0);
+console.log("20 !== 30",20!==30);
+console.log("True == 1",true == 1);
+console.log("undefined == nul",undefined==null);
+
+
+console.log("Assigment Operators");
+console.log("---------------------");
+
+let x = 10;
+x += 5;
+console.log("Addition",x);
+
+let y = 10;
+y -= 3;
+console.log("Subtraction",y);
+
+let m = 10;
+m*= 2;
+console.log("Multiply",m);
+
+let d =10;
+d/=4;
+console.log("divide",d);
+
+let r = 10;
+r%= 2;
+console.log("reminder",r);
+
+console.log("Ternary Operator");
+console.log("-------------------");
+
+let number = 13;
+console.log(number % 2 === 0 ?  `${number} isEven`:`${number} is Odd`);
+
+console.log("Operator Precedence");
+console.log("-------------------");
+
+
+let Precedence = 10+5*2-(3/3)+8;
+console.log("10+5*2-(3/3)+8 =",Precedence);
+
+console.log("Increament and Decreament Operators");
+console.log("------------------------------------");
+
+let count = 10;
+console.log(++count);
+
+let count2 =10;
+console.log(--count2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
